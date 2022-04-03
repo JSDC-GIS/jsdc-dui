41f691056c51c46e76b084fcf796a86d2019133e
andy.chen <andy10030015@gmail.com>
2022-04-03 14:23:56 +0800

    fix: enable test

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# jsdc-dui
+<h1 align="center">JSDC RUI</h1>
+
+---
+
+
+## Installation
+JSDC RUI is **not** available as an npm package.
+```
+npm install https://github.com/60723005l/jsdc-RUI.git
+
+// to install a tag version named 0.3.1
+npm install https://github.com/60723005l/jsdc-RUI.git#0.3.1
+```
+
+## Usage
+
+Components should be imported from `jsdc-rui/dist/components/...`
+
+example
+```tsx
+import React from 'react';
+import MapViewContainer from 'jsdc-rui/dist/components/MapViewContainer';
+import MenuList from 'jsdc-rui/dist/components/LeftMenuBar/MenuList';
+import headerMbImg from './testAsset/logo-m.jpg'
+import headerBImg from './testAsset/logo-d.jpg'
+import useSwitch from 'jsdc-rui/dist/utils/useSwitch';
+import LayerMenuItem from 'jsdc-rui/dist/components/LeftMenuBar/Layer/LayerMenuItem';
+import WeatherMenuItem from 'jsdc-rui/dist/components/LeftMenuBar/Weather/WeatherMenuItem';
+
+type MenuItemType = {
+  id: string,
+  name: string
+}
+
+function App() {
+  const mainMenuItems = [
+    { id: '地圖圖層', name: '地圖圖層' },
+    { id: '氣象預測', name: '氣象預測' }
+  ]
+  const { switchById, activeId } = useSwitch<MenuItemType>([...mainMenuItems])
+  const switcherAction = (id: string) => {
+    return {
+      onClick: () => switchById(id),
+      onClose: () => switchById(undefined)
+    }
+  }
+  return (
+    <MapViewContainer
+      headerImgSrc={headerMbImg}
+      menuChildren={(
+        <MenuList
+          title='橫越屏東縣春日鄉和' subtitle='臺東縣大武鄉的浸水營古道'
+          headerImg={headerBImg} headerMBImg={headerMbImg}>
+          <>
+            <LayerMenuItem active={activeId === '地圖圖層'} {...switcherAction('地圖圖層')}/>
+            <WeatherMenuItem active={activeId === '氣象預測'} {...switcherAction('氣象預測')}
+              locations={[
+                { county: '屏東縣', town: '枋寮鄉' },
+                { county: '屏東縣', town: '春日鄉' }
+              ]}
+              token='CWB-232A270E-12F1-4381-B9F2-DF2D2670A077'/>
+          </>
+        </MenuList>
+      )}/>
+  );
+}
+
+export default App;
+```
+
+#### ICONS
+Icons can be imported from `jsdc-rui/dist/icon`.
+All icons return src value, those icon assets was host by jsdc server.
+
+```tsx
+import icon from 'jsdc-rui/dist/icon'
+
+//activable icons
+icon.activable.layer
+icon.activable.layerActive
+
+//legend icons
+icon.legend.人文地景.default
+icon.legend.人文地景.active
+
+//credit icons
+icon.credit.qrcode
+
+const App = () => {
+  return <img src={icon.activable.layer} />
+}
+```
---
+++ src/index.tsx
@@ -0,0 +1,131 @@
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+import React, { useContext, useEffect, useState } from 'react';
+import { JSDCContext, JSDCProvider } from './JSDC/Context'
+import JSDC from './JSDC';
+import { LayerApiRespVectorProps } from './JSDC/Dguidewalks/ApiProvider';
+import Leaflet, { GeoJSON, Marker } from 'leaflet'
+import { IRuiContextProviderProps, RuiContext, RuiContextProvider } from './components/Context';
+import DguideWalksApp from './components/DguideWalksApp';
+import Dialog from './components/Dialog';
+import MenuItemWithDialog from './components/LeftMenuBar/MenuList/MenuItemWithDialog';
+import icon, { baseUrl } from './icon';
+import './style/index.css'
+import { DguidewalksContext, DguidewalksProvider } from './JSDC/Dguidewalks/Context';
+
+
+const ruiConfigProps: IRuiContextProviderProps = {
+  sidebarTitle: '標題1',
+  sidebarSubtitle: '標題2',
+  credit: '本平臺由智紳數位文化事業有限公司建置。若有其他利用或授權需求請洽【智紳數位文化事業】Facebook粉絲專頁。',
+  headerMBImgSrc: '',
+  headerDImgSrc: '',
+  menuSwitchItems: [{ id: '景點打卡', name: '景點打卡' }],
+  weatherConfig: {
+    token: 'CWB-232A270E-12F1-4381-B9F2-DF2D2670A077',
+    locations: [
+      { county: '屏東縣', town: '枋寮鄉' },
+      { county: '屏東縣', town: '春日鄉' }
+    ]},
+  legendConfig: {
+    activeLegends: ['歷史建物', '聚落', '紀念指標']
+  },
+  themeConfig: {}
+}
+
+const getRouteColorByType = (type: string) => {
+  switch (type) {
+    case '1':
+      return '#4aca69'
+    case '2':
+      return '#4aca69'
+    default:
+      return 'blue'
+  }
+}
+
+const getPOIIcon = (type: string) => {
+  return Leaflet.icon({ iconUrl: `${baseUrl}map_icons/type${type}.png`, iconSize: [30, 40] })
+}
+
+function App() {
+  const { Jsdc } = useContext(JSDCContext)
+  const rui = useContext(RuiContext)
+  const { dgw } = useContext(DguidewalksContext)
+  const [open, setopen] = useState(false)
+  const [title, settitle] = useState<string>()
+
+  const init = async () => {
+    await Jsdc.asyncViewer
+    const layerController = Jsdc.Controller.get('Layer')
+
+    layerController
+      .getByName<GeoJSON>('浸水營古道數位走讀示範路線')
+      ?.forEachLayerAsGeoJSON<any, LayerApiRespVectorProps>(
+        (layer, properties) => layer.setStyle({ color: getRouteColorByType(properties.type)})
+      )
+
+    layerController
+      .getByName<GeoJSON>('浸水營古道數位走讀示範景點')
+      ?.forEachLayerAsGeoJSON<any, LayerApiRespVectorProps>(
+        (layer: Marker, properties) => {
+          layer.setIcon(getPOIIcon(properties.type)!)
+          layer.on('click', () => {
+            setopen(true)
+            settitle(properties.name)
+          })
+        })
+
+  }
+  useEffect(() => {
+    (window as any).JSDC = Jsdc
+    dgw.gisDataLoadEvent.addEventListener(init)
+  }, [])
+  return (
+    
+        <>
+          <DguideWalksApp
+            mainMenuChildren={
+              <MenuItemWithDialog
+                startIconSrc={icon.activable.info}
+                startIconSrcActive={icon.activable.infoActive}
+                title='景點打卡'
+                active={rui.activeMenuId === '景點打卡'} {...rui.menuSwitcherAction('景點打卡')}>
+                  <div>打卡</div>
+              </MenuItemWithDialog>
+            }/>
+          <Dialog title={title} open={open} onClose={() => setopen(false)}/>
+        </>
+      
+  );
+}
+
+const AppWrapper: React.FC = () => {
+  const [Jsdc] = useState(new JSDC('s0002'))
+  return (
+    <JSDCProvider Jsdc={Jsdc}>
+      <DguidewalksProvider
+        Jsdc={Jsdc}
+        activeBasemaps={['臺灣通用電子地圖(灰階)', '正射影像圖(通用)', '嘉南大圳平圖(1933)', '臺灣經建1版地形圖']}
+        layersHiddenFromUI={['浸水營古道數位走讀示範景點', '浸水營古道數位走讀示範路線']}
+        layersShowOnMapByDefault={['臺灣通用電子地圖(灰階)', '浸水營古道數位走讀示範景點', '浸水營古道數位走讀示範路線']}>
+        <RuiContextProvider {...ruiConfigProps}>
+          <App/>
+        </RuiContextProvider>
+      </DguidewalksProvider>
+    </JSDCProvider>
+  )
+}
+
+export default AppWrapper;
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
