79e8df15c67769fadac8dd70cd527db43ee02193
creticulata <maple51327@gmail.com>
2025-10-15 15:08:25 +0800

    feat: rollup i18n

--- dist/i18n/locales/en.json.js
+++ dist/i18n/locales/en.json.js
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 var t={mapLayer:"Map Layers",sceneIntro:"Attractions",weatherForecast:"Forecast",legendDescription:"Legend",routeIntro:"Introduction",aboutPlatform:"About"},e={text:"Copyright Ⓒ {{year}}",maintenance:"Platform Maintenance | JRSHEN Digital Culture Co., Ltd. /",design:"Design by someDesign"},o={title:"Visitor Count"},n={menu:t,copyright:e,visitorCount:o};export{e as copyright,n as default,t as menu,o as visitorCount};
+=======
+var e={mapLayer:"Map Layers",sceneIntro:"Attractions",weatherForecast:"Weather Forecast",legendDescription:"Legend",routeIntro:"Route Introduction",aboutPlatform:"About Platform"},t={text:"Copyright Ⓒ {{year}}",maintenance:"Platform Maintenance | JRSHEN Digital Culture Co., Ltd. /",design:"Design by someDesign"},a={menu:e,copyright:t};export{t as copyright,a as default,e as menu};
+>>>>>>> 7c49023 (feat: rollup i18n)
 //# sourceMappingURL=en.json.js.map
