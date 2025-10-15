929033925a59afd287b327eed6632132d47de6ee
creticulata <maple51327@gmail.com>
2025-10-15 15:50:14 +0800

    feat: add visitor count to i18n

--- src/components/VisitorCount/index.tsx
+++ src/components/VisitorCount/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { counter } from '../../icon'
+import { useTranslation } from 'react-i18next'
 import './index.scss'
 
 export interface IVisitorCountProps {
@@ -7,10 +8,11 @@ export interface IVisitorCountProps {
 }
 
 const VisitorCount = ({ value }: IVisitorCountProps) => {
+  const { t } = useTranslation()
   const displayValue = String(value).padStart(10, '0')
   return (
     <div className="dui-VisitorCount">
-      <div className="dui-VisitorCount-title">瀏覽人次</div>
+      <div className="dui-VisitorCount-title">{t('visitorCount.title')}</div>
       <div className="dui-VisitorCount-display">
         {displayValue.split('').map((val, key) => (
           <img key={key} src={counter[val]} alt={String(key)} />
