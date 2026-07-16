78dce9888618149581e01f217661908aa9b50ed5
creticulata <maple51327@gmail.com>
2026-07-16 15:38:36 +0800

    fix: change Setting icon color

--- src/components/Icons/Setting.tsx
+++ src/components/Icons/Setting.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { IActivableProps } from './types'
-import { inactiveColor, activeColor } from './common/ActivableColor'
+import {
+  inactiveColor,
+  activeColor,
+  activeAccentColor,
+} from './common/ActivableColor'
 
 const Setting = ({ active }: IActivableProps) => {
-  const fill = active ? activeColor : inactiveColor
+  // match Hamburger: bars use activeColor, knobs (dots) use activeAccentColor
+  const barFill = active ? activeColor : inactiveColor
+  const dotFill = active ? activeAccentColor : inactiveColor
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -12,27 +18,27 @@ const Setting = ({ active }: IActivableProps) => {
       viewBox="0 0 32 32"
     >
       <path
-        style={{ fill }}
+        style={{ fill: barFill }}
         d="M4.06,29.7a1.014,1.014,0,0,1-1.015-1.015V3.312a1.015,1.015,0,0,1,2.03,0V28.687A1.014,1.014,0,0,1,4.06,29.7"
       />
       <path
-        style={{ fill: active ? activeColor : inactiveColor }}
+        style={{ fill: dotFill }}
         d="M8.12,19.9a4.06,4.06,0,1,1-4.06-4.06A4.06,4.06,0,0,1,8.12,19.9"
       />
       <path
-        style={{ fill }}
+        style={{ fill: barFill }}
         d="M16,29.7a1.014,1.014,0,0,1-1.015-1.015V3.313a1.015,1.015,0,1,1,2.03,0V28.688A1.014,1.014,0,0,1,16,29.7"
       />
       <path
-        style={{ fill: active ? activeColor : inactiveColor }}
+        style={{ fill: dotFill }}
         d="M20.06,10.335A4.06,4.06,0,1,1,16,6.275a4.06,4.06,0,0,1,4.06,4.06"
       />
       <path
-        style={{ fill }}
+        style={{ fill: barFill }}
         d="M27.94,29.7a1.014,1.014,0,0,1-1.015-1.015V3.312a1.015,1.015,0,0,1,2.03,0V28.687A1.014,1.014,0,0,1,27.94,29.7"
       />
       <path
-        style={{ fill: active ? activeColor : inactiveColor }}
+        style={{ fill: dotFill }}
         d="M32,19.9a4.06,4.06,0,1,1-4.06-4.06A4.06,4.06,0,0,1,32,19.9"
       />
     </svg>
